f63cfed4b9a553fc53f3205df6f48f1bf7f78e1b
student <Hamdi Riady>
2017-12-19 14:47:35 +0700

    Modify Source Code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Hello World again");
+		System.out.println("Hello World again for testing !");
 		System.out.println("Hae!");
 	}
 }
